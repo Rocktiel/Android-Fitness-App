a3dbf0b5bda8e03eef135dd7e9e63b2e56384e27
Xpmian <teofilo_1967@outlook.com.tr>
2023-05-03 12:58:49 +0300

    ikinci commit

---
+++ app/src/main/java/com/example/mobilprogramlamaproje/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.example.mobilprogramlamaproje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        System.out.println("asdasdasdasd");
+        System.out.println("samimal");
+    }
+}
